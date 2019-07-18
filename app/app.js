5468071aa9a989853a7195c2291a3a0d10a0c0a3
jl-fb <jlborgh@gmail.com>
2019-07-18 10:00:21 -0300

    Fix verificaçao da chamada de Estado

--- app/app.js
+++ app/app.js
@@ -11,36 +11,46 @@ const nomes = estados.map(p => {
     return nome
 })
 
-// Capturando o evento submit do formulário do index
-$('#form').submit((e) => {
-    e.preventDefault(); //Evitando o carregamento automático da página após o click
-    const nome = $('input').val().toUpperCase()
 
-    function maior(valor) {
-        return valor >= 0.5
-    }
+const retornarEstado = function () {
+    // Capturando o evento submit do formulário do index
+    $('#form').submit((e) => {
+        e.preventDefault(); //Evitando o carregamento automático da página após o click
+        const nome = $('input').val().toUpperCase()
 
-    // Realizando verificação para resgatar os dados do Estado digitado 
-    for (let i in nomes) {
-        // armazenando índice de igualdade em uma variável
-        let nomesComparados = comparaString.compareTwoStrings(nome, nomes[i])
+        // Realizando verificação para resgatar os dados do Estado digitado 
+        let nomesComparados = []
+        let estado = {}
 
-        if (nomesComparados >= 0.5) {
-            let estado = estados[i]
+        for (let i in nomes) {
+            // armazenando índice de igualdade em uma variável
+            nomesComparados = comparaString.compareTwoStrings(nome, nomes[i])
+            if (nomesComparados >= 0.5) {
+                estado = estados[i]
+            }
+        }
+        console.log(estado)
+        if (estado.nome === undefined) {
+            const erro = $('<td>').html(`Estado não escontrado. Estado digitado ${nome}`)
+            $('#erro').empty()
+            $('#dados-estado').empty()
+            $('#erro').append(erro).addClass('d-block')
 
+        } else {
             const tdSigla = $('<td>').html(`${estado.sigla}`)
             const tdNome = $('<td>').html(`${estado.nome}`)
             const tdCapital = $('<td>').html(`${estado.capital}`)
             const tdRegiao = $('<td>').html(`${estado.regiao}`)
 
             $('#dados-estado').empty()
-
+            $('#erro').addClass('d-none')
+            $('#erro').empty()
             $('#dados-estado').append(tdSigla).append(tdNome)
                 .append(tdCapital).append(tdRegiao)
-        } else {
-            const erro = $('<td>').html(`Estado não escontrado. Estado digitado ${nome}`)
-            $('#erro').empty()
-            $('#erro').append(erro).addClass('d-block')
+
         }
-    }
-})
+
+    })
+}
+
+retornarEstado()
